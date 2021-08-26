c55e8d21e7d967a365cdffb3ea4445a93c24b4c0
IshanJawade <ishankjawade@gmail.com>
2021-08-26 16:31:12 +0530

    Chcnages done in main Program

--- stackinJava.java
+++ stackinJava.java
@@ -1,56 +1,55 @@
-public class stackinJava{
+// Stack implementation in Java
 
-    static class stackImplementation{
-        int top = -1;
-        int capacity = 10;
-        int arr[] = new int[capacity];
+class stackImplementation{
+    int top = -1;
+    int capacity = 10;
+    int arr[] = new int[capacity];
 
-        public boolean isEmpty() {
-            return top == -1;
-        }
-        public boolean isFull() {
-            return top == capacity - 1;
-        }
-        public int push (int data) {
-            if ( isFull() ){
-                System.out.println("Stack is Full !");
-                return 0;
-            }
-            else{
-                return arr[++top]=data;
-            }
-        }
-        public int pop () {
-            if (isEmpty()) {
-                System.out.println("Stack is Empty !");
-                return 0;
-            }
-            else {
-                return arr[--top];
-            }
-        }
-        public int peek() {
-            return arr[top];
-        }
-
+    public boolean isEmpty() {
+        return top == -1;
     }
+    public boolean isFull() {
+        return top == capacity - 1;
+    }
+    public int push (int data) {
+        if ( isFull() ){
+            System.out.println("Stack is Full !");
+            return 0;
+        }
+        else{
+            return arr[++top]=data;
+        }
+    }
+    public int pop () {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty !");
+            return 0;
+        }
+        else {
+            return arr[--top];
+        }
+    }
+    public int peek() {
+        return arr[top];
+    }
+
+}
+public class stackinJava{
     public static void main(String[] args) {
         
-            stackImplementation st = new stackImplementation();
-            st.push(10);
-            st.push(123);
-            st.push(145);
-            st.push(1789);
-            st.push(3434);
-            st.push(167);
-            System.out.println(st);
-            st.pop();
-            st.pop();
-            st.pop();
-            st.push(3000);
-            System.out.println(st.peek());
-            System.out.println(st.isEmpty());
-
-        
+        stackImplementation st = new stackImplementation();
+        st.push(10);
+        st.push(123);
+        st.push(145);
+        st.push(1789);
+        st.push(3434);
+        st.push(167);
+        System.out.println(st);
+        st.pop();
+        st.pop();
+        st.pop();
+        st.push(3000);
+        System.out.println(st.peek());
+        System.out.println(st.isEmpty());
     }
 }
