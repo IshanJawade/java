ee12f82c1ee0ac06bce3040a697858177da390d7
IshanJawade <ishankjawade@gmail.com>
2021-08-26 15:39:29 +0530

    String reversal code added

--- stringReversal.java
+++ stringReversal.java
@@ -1,8 +1,8 @@
-class stringReversal{
+public class stringReversal{
     public static void main(String[] args) {
-        int arr[] = {12,45,67,89,63,73,75};
-        for (int i : arr){
-            System.out.println(i);
-        }
-    }  
- }
+        String str = "Chapla, lightin and Gadya";
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        System.out.println(sb);
+    }
+}  
