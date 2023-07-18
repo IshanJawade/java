97f2f30adfc79c0be38a6a3c7f94c7bd2f38078c
IshanJawade <ishankjawade@gmail.com>
2023-07-19 01:41:53 +0530

    patterns are added in basic codes

--- basicCodes/patterns.java
+++ basicCodes/patterns.java
@@ -1,6 +1,22 @@
-class javaTest{
-    
+public class patterns {
     public static void main(String args[]){
+
+        // half piramid
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // inverted half Piramid
+        for(int i = 5; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
         // Piramid
         int num = 7; // only accepts odd numbers 
         int sum = 0;
@@ -17,9 +33,8 @@ class javaTest{
                 System.out.print((j == avg) || (j >= l && j <= r) ? "*" : " ");
             }
             l -=  1; r  +=  1;
-        
             System.out.println();
         }
-    }
 
+    }
 }
